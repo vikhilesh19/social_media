4125e03aac085331207646d44edf9c289b7f4153
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-07-02 15:36:30 +0530

    data fetched after loading the page

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,21 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/postListStore";
 import Welcomemsg from "./Welcomemsg";
 const Postlist = () => {
   const { postList, addinitialposts } = useContext(PostListData);
-  const handlegetpostclick = () => {
+  const [datafetch, setdatafetch] = useState(false);
+  if (!datafetch) {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addinitialposts(data.posts);
       });
-  };
+    setdatafetch(true);
+  }
+  const handlegetpostclick = () => {};
   return (
     <>
-      {postList.length === 0 && (
-        <Welcomemsg ongetpostclick={handlegetpostclick} />
-      )}
+      {postList.length === 0 && <Welcomemsg />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- src/components/Welcomemsg.jsx
+++ src/components/Welcomemsg.jsx
@@ -2,13 +2,6 @@ const Welcomemsg = ({ ongetpostclick }) => {
   return (
     <center className="Welcomemsg">
       <h1>There are no posts.</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={ongetpostclick}
-      >
-        Get Post From Server
-      </button>
     </center>
   );
 };
