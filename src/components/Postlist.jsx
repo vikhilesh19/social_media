c622f3205ccb9e22cf8e95e028185f06fe4bad22
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-07-02 16:07:11 +0530

    useeffect

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,18 +1,16 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/postListStore";
 import Welcomemsg from "./Welcomemsg";
 const Postlist = () => {
   const { postList, addinitialposts } = useContext(PostListData);
-  const [datafetch, setdatafetch] = useState(false);
-  if (!datafetch) {
+  useEffect(() => {
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addinitialposts(data.posts);
       });
-    setdatafetch(true);
-  }
+  }, []);
   const handlegetpostclick = () => {};
   return (
     <>
