f3146703cd5eb8d0ce87c43ad5b4b6d24922f359
vikhilesh19 <cs24b1077@iiitdm.ac.in>
2026-07-02 20:49:21 +0530

    Loading spinner

--- src/components/Postlist.jsx
+++ src/components/Postlist.jsx
@@ -1,23 +1,26 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/postListStore";
 import Welcomemsg from "./Welcomemsg";
+import LoadingSpinner from "./LoadingSpinner";
 const Postlist = () => {
   const { postList, addinitialposts } = useContext(PostListData);
+  const [fetching, setfetching] = useState(false);
   useEffect(() => {
+    setfetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addinitialposts(data.posts);
+        setfetching(false);
       });
   }, []);
   const handlegetpostclick = () => {};
   return (
     <>
-      {postList.length === 0 && <Welcomemsg />}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <Welcomemsg />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
